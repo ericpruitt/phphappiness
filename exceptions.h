f602b778dd37d5234ec73cc5e23c91275522beba
Eric Pruitt <eric.pruitt@gmail.com>
2013-06-01 18:50:36 -0500

    Exceptions for socket functions

--- exceptions.h
+++ exceptions.h
@@ -9,11 +9,15 @@
     (($__ = test) ? THROW(e, #test . " error $__") : \
     false)) ?: $_)
 
-#define json_encode(v) \
+//                               JSON Functions
+
+#define json_encode(...) \
     THROWER(json_encode, (__VA_ARGS__), json_last_error(), DomainException)
 
 #define json_decode(...) \
     THROWER(json_decode, (__VA_ARGS__), json_last_error(), DomainException)
+
+//                               PCRE Functions
 
 #define preg_match(...) \
     THROWER(preg_match, (__VA_ARGS__), preg_last_error(), RuntimeException)
@@ -23,5 +27,28 @@
 
 #define preg_match_replace(...) \
     THROWER(preg_match_all, (__VA_ARGS__), preg_last_error(), RuntimeException)
+
+//                              Socket Functions
+
+// Generic macro for socket functions to test whether or not an error occurred.
+#define SOCKET_THROWER(fname, ...) ($_ = @fname(__VA_ARGS__) ?: \
+    (array($__ = socket_last_error(), $__ = socket_strerror($__), \
+    socket_clear_error(), THROW(Exception, $__))))
+
+#define socket_accept(...) SOCKET_THROWER(socket_accept, __VA_ARGS__)
+#define socket_bind(...) SOCKET_THROWER(socket_bind, __VA_ARGS__)
+#define socket_connect(...) SOCKET_THROWER(socket_connect, __VA_ARGS__)
+#define socket_create_listen(...) SOCKET_THROWER(socket_create_listen, __VA_ARGS__)
+#define socket_create_pair(...) SOCKET_THROWER(socket_create_pair, __VA_ARGS__)
+#define socket_create(...) SOCKET_THROWER(socket_create, __VA_ARGS__)
+#define socket_listen(...) SOCKET_THROWER(socket_listen, __VA_ARGS__)
+#define socket_read(...) SOCKET_THROWER(socket_read, __VA_ARGS__)
+#define socket_recv(...) SOCKET_THROWER(socket_recv, __VA_ARGS__)
+#define socket_recvfrom(...) SOCKET_THROWER(socket_recvfrom, __VA_ARGS__)
+#define socket_select(...) SOCKET_THROWER(socket_select, __VA_ARGS__)
+#define socket_send(...) SOCKET_THROWER(socket_send, __VA_ARGS__)
+#define socket_sendto(...) SOCKET_THROWER(socket_sendto, __VA_ARGS__)
+#define socket_set_option(...) SOCKET_THROWER(socket_set_option, __VA_ARGS__)
+#define socket_write(...) SOCKET_THROWER(socket_write, __VA_ARGS__)
 
 #endif
