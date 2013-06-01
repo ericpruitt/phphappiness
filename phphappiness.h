44488e34e749b67b1f6deec7019c4da6e3fa6786
Eric Pruitt <eric.pruitt@gmail.com>
2013-05-31 22:49:56 -0500

    Make explode, __lambda_func, and searches happy
    - Worked on issues 9, 35 and 39 of phpsadness.com

--- phphappiness.h
+++ phphappiness.h
@@ -29,4 +29,27 @@
 
 #define array_fill(s, n, v) (!n ? array() : array_fill(s, n, v))
 
+// Issue 39 (http://phpsadness.com/sad/39):
+// Declaring a function called __lambda_func() completely breaks
+// create_function()
+
+#define __lambda_func ___lambda_func
+
+// Issue 35 (http://phpsadness.com/sad/35):
+// The explode() function won't split with empty string
+
+#define explode(d, s) (strlen(d) ? explode(d, s) : str_split(s))
+
+//                         Needle / Haystack problems
+
+// Issue 9 (http://phpsadness.com/sad/9):
+// Order of arguments (array/string search)
+
+// Let's just make everything haystack / needle for consistency.
+// TODO: Use __VA_ARGS__ and nested macros for third, optional argument.
+
+#define array_key_exists(haystack, needle) array_key_exists(needle, haystack)
+#define in_array(haystack, needle) in_array(haystack, needle, true)
+#define array_search(haystack, needle) array_search(haystack, needle, true)
+
 #endif
