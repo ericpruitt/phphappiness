a5a4a41f742d6a7946966a6b0a95fae06c071371
Eric Pruitt <eric.pruitt@gmail.com>
2013-06-04 20:45:30 -0500

    Began documenting macros in DOCUMENTATION.md

--- PHPHappiness/headers/argtypes.h
+++ PHPHappiness/headers/argtypes.h
@@ -1,27 +1,3 @@
-/* This header provides macros for strict PHP argument type enforcement. Due to
- * the implementation of this macro, function definitions are tad different. To
- * declare a function with strictly typed arguments, you must use the FUNCTION
- * macro, and each of the function arguments is defined with the REQ macro
- * which expects the variable type and variable name. The end of the function
- * must also be terminated with the ENDFUNCTION macro. Here's a simple function
- * that requires strings for concatenation:
- *
- *  FUNCTION(concatenate, REQ(string, $leftstring); REQ(string, $rightstring))
- *  {
- *      return $leftstring . $rightstring;
- *  }
- *  ENDFUNCTION
- *
- * Note that the function arguments must be separated with semicolons instead
- * of commas. Calls to the function remain the same, and any attempts to call
- * the function with invalid types will result in an InvalidArgumentException
- * being thrown.
- *
- * When specifying type restrictions for numerics, integers will be accepted
- * for functions that specify "float", "double", or "real" as the argument
- * type. However, the reverse is not true: if "integer" is specified as the
- * argument type, floating point numbers will be rejected by the function.
- */
 #ifndef ARGTYPES_H
 #define ARGTYPES_H
 
